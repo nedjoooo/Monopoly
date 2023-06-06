7a5f0792f4fd38086494083f98d8488187527e23
Nedjo <nedjmi.ibryamov@gmail.com>
2023-06-06 19:22:50 +0300

    Creating Project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    String[][] monopolyTable = {};
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void printTable() {
+
+    }
+}
